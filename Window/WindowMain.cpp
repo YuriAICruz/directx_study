e741e61a1152ffc90dfa8cd8488e464ab1875a66
Yuri Cruz <yuriaicruz@gmail.com>
2023-10-06 11:00:49 -0300

    getMessage and Win Procedure

--- Window/WindowMain.cpp
+++ Window/WindowMain.cpp
@@ -1,5 +1,16 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CLOSE:
+        PostQuitMessage(0);
+        break;
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
     HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
@@ -11,7 +22,7 @@ int CALLBACK WinMain(
     WNDCLASSEX wc = {0};
     wc.cbSize = sizeof(wc);
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = WndProc;
     wc.cbClsExtra = 0;
     wc.hInstance = hInstance;
     wc.hIcon = nullptr;
@@ -26,7 +37,7 @@ int CALLBACK WinMain(
         0,
         pClassName,
         pWindowName,
-        WS_CAPTION | WS_MINIMIZEBOX,
+        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
         200, 200,
         640, 480,
         nullptr,
@@ -34,8 +45,19 @@ int CALLBACK WinMain(
         hInstance,
         nullptr);
     ShowWindow(hWnd, SW_SHOW);
-    while (true)
+
+    MSG msg;
+    BOOL gResult;
+    while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
     {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
-    return 0;
+
+    if(gResult == -1)
+    {
+        return gResult;
+    }
+    
+    return msg.wParam;
 }
