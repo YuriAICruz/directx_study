078c371a90c738d4702bc6b5d597a820a68baa1f
Yuri Cruz <yuriaicruz@gmail.com>
2023-10-06 09:58:30 -0300

    Simple window ex creation

--- Window/WindowMain.cpp
+++ Window/WindowMain.cpp
@@ -1,11 +1,41 @@
 #include <Windows.h>
 
 int CALLBACK WinMain(
-    HINSTANCE hinstance,
+    HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     LPSTR lpCmdLine,
     int nCmdShow)
 {
-    
+    const LPCWSTR pClassName = L"d3dWindow";
+    const LPCWSTR pWindowName = L"Window";
+    WNDCLASSEX wc = {0};
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = DefWindowProc;
+    wc.cbClsExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = nullptr;
+    wc.hCursor = nullptr;
+    wc.hbrBackground = nullptr;
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = pClassName;
+    wc.hIconSm = nullptr;
+
+    RegisterClassEx(&wc);
+    HWND hWnd = CreateWindowEx(
+        0,
+        pClassName,
+        pWindowName,
+        WS_CAPTION | WS_MINIMIZEBOX,
+        200, 200,
+        640, 480,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr);
+    ShowWindow(hWnd, SW_SHOW);
+    while (true)
+    {
+    }
     return 0;
 }
