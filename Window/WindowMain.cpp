8fc7657b674560c7563540c36c32f4370d10108d
Yuri Cruz <yuriaicruz@gmail.com>
2023-10-09 11:32:59 -0300

    window class encapsulation

--- Window/WindowMain.cpp
+++ Window/WindowMain.cpp
@@ -1,53 +1,21 @@
-#include "ModWindows.h"
+#include <iostream>
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch (msg)
-    {
-    case WM_CLOSE:
-        PostQuitMessage(0);
-        break;
-    }
-    return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+#include "Test.h"
+#include "src/Window.h"
 
-int CALLBACK WinMain(
+int WINAPI WinMain(
     HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     LPSTR lpCmdLine,
     int nCmdShow)
 {
-    const LPCWSTR pClassName = L"d3dWindow";
-    const LPCWSTR pWindowName = L"Window";
-    WNDCLASSEX wc = {0};
-    wc.cbSize = sizeof(wc);
-    wc.style = CS_OWNDC;
-    wc.lpfnWndProc = WndProc;
-    wc.cbClsExtra = 0;
-    wc.hInstance = hInstance;
-    wc.hIcon = nullptr;
-    wc.hCursor = nullptr;
-    wc.hbrBackground = nullptr;
-    wc.lpszMenuName = nullptr;
-    wc.lpszClassName = pClassName;
-    wc.hIconSm = nullptr;
-
-    RegisterClassEx(&wc);
-    HWND hWnd = CreateWindowEx(
-        0,
-        pClassName,
-        pWindowName,
-        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-        200, 200,
-        640, 480,
-        nullptr,
-        nullptr,
-        hInstance,
-        nullptr);
-    ShowWindow(hWnd, SW_SHOW);
-
+    Window wnd(800,300, L"Window View");
     MSG msg;
     BOOL gResult;
+
+    Test test = {};
+    std::cout << test.Tester();
+
     while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
     {
         TranslateMessage(&msg);
---
+++ Window/src/Window.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "ModWindows.h"
+
+class Window
+{
+private:
+    class WindowClass
+    {
+    public:
+        static LPCWSTR GetName() noexcept;
+        static HINSTANCE GetInstance() noexcept;
+    private:
+        WindowClass() noexcept;
+        ~WindowClass();
+        WindowClass(const WindowClass&) = delete;
+        WindowClass& operator=(const WindowClass&) = delete;
+        static constexpr LPCWSTR wndClassName = L"Direct3D Engine Window";
+        static WindowClass wndClass;
+        HINSTANCE hInst;
+    };
+
+public:
+    Window(int width, int height, LPCWSTR name) noexcept;
+    ~Window();
+    Window(const Window&) = delete;
+    Window& operator=(const Window&) = delete;
+private:
+    static LRESULT WINAPI HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+    static LRESULT WINAPI HandleMsgTrunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+    LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+
+    int width;
+    int height;
+    HWND hWnd;
+};
