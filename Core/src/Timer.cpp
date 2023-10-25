8e482117dd459551cc5c5e50a4659c17b37ff458
Yuri Cruz <yuriaicruz@gmail.com>
2023-10-25 10:47:16 -0300

    remove using namespace

--- Core/src/Timer.cpp
+++ Core/src/Timer.cpp
@@ -1,26 +1,24 @@
 ﻿#include "Timer.h"
 
-using namespace std::chrono;
-
 Timer::Timer() noexcept
 {
-    start = steady_clock::now();
-    last = steady_clock::now();
+    start = std::chrono::steady_clock::now();
+    last = std::chrono::steady_clock::now();
 }
 
 float Timer::Mark() noexcept
 {
     const auto old = last;
-    last = steady_clock::now();
-    const duration<float> frameTime = last - old;
+    last = std::chrono::steady_clock::now();
+    const std::chrono::duration<float> frameTime = last - old;
     return frameTime.count();
 }
 
 float Timer::Peek() const noexcept
 {
-    return duration<float>(steady_clock::now() - last).count();
+    return std::chrono::duration<float>(std::chrono::steady_clock::now() - last).count();
 }
 float Timer::FromStart() const noexcept
 {
-    return duration<float>(steady_clock::now() - start).count();
+    return std::chrono::duration<float>(std::chrono::steady_clock::now() - start).count();
 }
